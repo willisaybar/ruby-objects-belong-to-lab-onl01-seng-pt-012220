0120b7600c98def627788a15434073fbecd1d565
Willis Aybar <willisaybar@gmail.com>
2020-02-25 01:38:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
   attr_accessor :title, :artist
 
-  def initialize(title, artist)
+  def initialize(title = "title", artist = "artist")
     @title =  title
     @artist = artist 
   end
