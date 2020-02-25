e3aba802380d139ca79729a1ec8bc93121dcf9b9
Willis Aybar <willisaybar@gmail.com>
2020-02-25 01:36:57 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,9 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
 
-  def initialize(title = "title")
+  def initialize(title = "title", author)
     @title = title
+    @author = author
   end
 
 end
